4b5a080a5cbd5aead035df39428e6b2733ee5ee3
marcohalloran <ohalloranmarc@hotmail.com>
2025-11-22 20:58:17 +0000

    Collect trains from all lines in core to one list in train

--- tfls/train.py
+++ tfls/train.py
@@ -1,5 +1,6 @@
 from tfls.line import get_line_by_id
 from tfls.custom_api import tfl_get
+from tfls.core import get_line_ids
 
 
 class Train:
@@ -52,41 +53,48 @@ class Train:
             self.long = self.from_station.lon
 
 def get_trains():
-    victoria_trains = tfl_get("/Line/victoria/Arrivals")
-    victoria_trains_parsed = []
+    line_ids = get_line_ids()
+    full_trains = []
+    for line in line_ids:
+        current_line_trains = tfl_get(f"/Line/{line}/Arrivals")
+     
+        current_line_trains_parsed = []
 
-    for train in victoria_trains:
-        current_location = train["currentLocation"].lower()
-        if "between" in current_location:
-            current_location1 = current_location.replace("between ","")
-            position = current_location1.split(" and ", 1)
-            if "platform" in position[0]:
-                position[0] = position[0].split('platform')[0]
-            elif "platform" in position[1]:
-                position[1] = position[1].split('platform')[0]
-            victoria_trains_parsed.append(position)
-        elif 'at platform' in current_location:
-            continue
-        elif 'at' in current_location:
-            position = current_location.split(" ", 1)[1]
-            if "platform" in position:
-                position = position.split("platform")[0]
-            victoria_trains_parsed.append([position, None])
-        elif 'approaching' in current_location:
-            position = current_location.split(" ", 1)[1]
-            if "platform" in position:
-                position = position.split("platform")[0]
-            victoria_trains_parsed.append([None, position])
+        for train in current_line_trains:
+            current_location = train["currentLocation"].lower()
+            if "between" in current_location:
+                current_location1 = current_location.replace("between ","")
+                position = current_location1.split(" and ", 1)
+                if "platform" in position[0]:
+                    position[0] = position[0].split('platform')[0]
+                elif "platform" in position[1]:
+                    position[1] = position[1].split('platform')[0]
+                current_line_trains_parsed.append(position)
+            elif 'at platform' in current_location:
+                continue
+            elif 'at' in current_location:
+                position = current_location.split(" ", 1)[1]
+                if "platform" in position:
+                    position = position.split("platform")[0]
+                current_line_trains_parsed.append([position, None])
+            elif 'approaching' in current_location:
+                position = current_location.split(" ", 1)[1]
+                if "platform" in position:
+                    position = position.split("platform")[0]
+                current_line_trains_parsed.append([None, position])
 
-    victoria_train_objs = []
-    for train in victoria_trains_parsed:
-        victoria_line = get_line_by_id('victoria')
-        if train[0] != None and train[1] != None:
-            victoria_train_objs.append(Train(victoria_line.get_station(train[0]), victoria_line.get_station(train[1]), 'victoria'))
-        elif train[0] == None:
-            victoria_train_objs.append(Train(None, victoria_line.get_station(train[1]), 'victoria'))
-        else:
-            victoria_train_objs.append(Train(victoria_line.get_station(train[0]), None, 'victoria'))
+    
+        current_line_train_objs = []
+        current_line = get_line_by_id(line)
+        for train in current_line_trains_parsed:
+            if train[0] != None and train[1] != None:
+                current_line_train_objs.append(Train(current_line.get_station(train[0]), current_line.get_station(train[1]), line))
+            elif train[0] == None:
+                current_line_train_objs.append(Train(None, current_line.get_station(train[1]), line))
+            else:
+                current_line_train_objs.append(Train(current_line.get_station(train[0]), None, line))
 
-    return victoria_train_objs
+        full_trains += current_line_train_objs
+
+    return full_trains
 
